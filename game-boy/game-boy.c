bf0a0c108c6a0bd603c660cb0563b1a73537a638
Alejandro <ale.deheza@gmail.com>
2020-05-03 06:54:41 -0300

    Separo el codigo en mas funciones para que se entienda mejor, ya se pueden mandar mensajes ingresando parametros por consola (sin usar el .hs), y ya se pueden mandar todos los tipos de mensajes desde el Gameboy. Falta generar la mayoria de los logs, y faltaria verificar que el Gameboy pueda mandar mensajes correctamente a otros modulos (lo probe con el Server del TP0 y funciona todo bien)

--- game-boy/game-boy.c
+++ game-boy/game-boy.c
@@ -3,38 +3,143 @@
 #include <cliente.h>
 #include "../utils/config.h"
 
-int main(void) {
+int main(int argc, char *argv[]) {
 
-	int conexion;
-	char* ip;
-	char* puerto;
-	char* log_file;
+	char* ip = NULL;
+	char* puerto = NULL;
+	t_log* logger = NULL;
+	t_config* config = NULL;
 
-	t_log* logger;
-	t_config* config;
+	verificarEntrada(argc);
+	int conexion = configuracionInicial(ip, puerto, &logger, &config, argv);
+	despacharMensaje(conexion, argc, argv);
+	//enviarAlgoParaProbar(conexion); //PARA PROBAR A MANO
+	terminar_programa(conexion, logger, config);
+	exit(0);
+}
 
-	config = leer_config("../game-boy.config");
+void verificarEntrada(int argc){
 
-	asignar_string_property(config, "IP_BROKER", &ip);
-	asignar_string_property(config, "PUERTO_BROKER", &puerto);
-	asignar_string_property(config, "LOG_FILE", &log_file);
+	if(argc == 1 || argc == 2 || argc == 3 ){
+		printf("Debe ingresar los argumentos con el siguiente formato:\n"
+		"./gameboy [PROCESO] [TIPO_MENSAJE] [ARGUMENTOS]*\n"
+		"o\n"
+		"./gameboy SUSCRIPTOR [COLA_DE_MENSAJES] [TIEMPO]\n");
+		exit(-1);
+	}
+	if(argc > 8){
+		printf("Ingresaste demasiados argumentos\n");
+		exit(-1);
+	}
+}
 
-	logger = log_create(log_file, "game-boy" , true, LOG_LEVEL_INFO);
+int configuracionInicial(char* ip, char* puerto, t_log** logger, t_config** config, char *argv[]){
+
+	char primerArg[TAMANIO_MAXIMO_ARGUMENTO];
+	char* log_file = NULL;
+	int conexion = 0;
+
+	*config = leer_config("../game-boy.config");
+	asignar_string_property(*config, "LOG_FILE", &log_file);
+	*logger = log_create(log_file, "game-boy" , true, LOG_LEVEL_INFO);
 
 	if(!log_file){
 		log_file = "game-boy.log";
 	}
+	printf("\nConfiguraciones:.\nLOG_FILE = %s\n", log_file);
+
+	strcpy(primerArg, argv[1]);
+
+	if(strcmp(primerArg,"SUSCRIPTOR")==0){
+		//suscribirAColaDeMensajes();			//TODO
+		}else if(strcmp(primerArg,"BROKER")==0){
+			destinoBroker(&ip, &puerto, *config);
+		}else if(strcmp(primerArg,"TEAM")==0){
+			destinoTeam(&ip, &puerto, *config);
+		}else if(strcmp(primerArg,"GAMECARD")==0){
+			destinoGamecard(&ip, &puerto, *config);
+		}else{
+			printf("El primer argumento es incorrecto\n");
+			exit(-1);
+			}
+
+		printf("IP = %s.\nPUERTO = %s.\n", ip, puerto);
 
 	if(!ip || !puerto){
-		log_info(logger, "Chequear archivo de configuracion");
+		log_info(*logger, "Chequear archivo de configuracion");
 		exit(-1);
 	}
 
-	printf("\nConfiguraciones:.\nIP = %s.\nPUERTO = %s\nLOG_FILE = %s.\n",ip, puerto, log_file);
-
 	conexion = crear_conexion( ip, puerto);
+	return conexion;
+}
 
-	char* nombre;
+void destinoBroker(char** ip, char** puerto, t_config* config){
+
+	asignar_string_property(config, "IP_BROKER", ip);
+	asignar_string_property(config, "PUERTO_BROKER", puerto);
+}
+
+void destinoTeam(char** ip, char** puerto, t_config* config){
+
+	asignar_string_property(config, "IP_TEAM", ip);
+	asignar_string_property(config, "PUERTO_TEAM", puerto);
+}
+
+void destinoGamecard(char** ip, char** puerto, t_config* config){
+
+	asignar_string_property(config, "IP_GAMECARD", ip);
+	asignar_string_property(config, "PUERTO_GAMECARD", puerto);
+}
+
+void despacharMensaje(int conexion, int argc, char *argv[]){
+
+	char segundoArg[TAMANIO_MAXIMO_ARGUMENTO];
+	char tercerArg[TAMANIO_MAXIMO_ARGUMENTO];
+	char cuartoArg[TAMANIO_MAXIMO_ARGUMENTO];
+	char quintoArg[TAMANIO_MAXIMO_ARGUMENTO];
+	char sextoArg[TAMANIO_MAXIMO_ARGUMENTO];
+	char septimoArg[TAMANIO_MAXIMO_ARGUMENTO];
+	char octavoArg[TAMANIO_MAXIMO_ARGUMENTO];
+
+	//tengo que pensar mejor esto
+	strcpy(segundoArg, argv[2]);
+	strcpy(tercerArg, argv[3]);
+	strcpy(cuartoArg, argv[4]);
+	strcpy(quintoArg, argv[5]);
+	strcpy(sextoArg, argv[6]);
+	//strcpy(septimoArg, argv[7]);
+	//strcpy(octavoArg, argv[8]);
+
+/*	//PARA PROBAR SIN CONSOLA
+	strcpy(primerArg, "BROKER");
+	strcpy(segundoArg, "NEW_POKEMON");
+	strcpy(tercerArg, "asd");
+	strcpy(cuartoArg, "1");
+	strcpy(quintoArg, "2");
+	strcpy(sextoArg, "3");
+*/
+
+	if(strcmp(segundoArg,"NEW_POKEMON")==0){
+		enviarNew(conexion, tercerArg, cuartoArg, quintoArg, sextoArg);
+		}else if(strcmp(segundoArg,"APPEARED_POKEMON")==0){
+			enviarAppeared(conexion, tercerArg, cuartoArg, quintoArg);
+		}else if(strcmp(segundoArg,"CATCH_POKEMON")==0){
+			enviarCatch(conexion, tercerArg, cuartoArg, quintoArg);
+		}else if(strcmp(segundoArg,"CAUGHT_POKEMON")==0){
+			enviarCaught(conexion, tercerArg);
+		}else if(strcmp(segundoArg,"GET_POKEMON")==0){
+			enviarGet(conexion, tercerArg);
+		}else{
+			printf("El segundo argumento es incorrecto\n");
+			exit(-1);
+		}
+}
+
+/*	//PARA PROBAR A MANO
+void enviarAlgoParaProbar(int conexion){
+
+	char* nombre = malloc(20);
 	int posx;
 	int posy;
 	int cantidad;
@@ -48,19 +153,56 @@ int main(void) {
 	scanf("%d", &cantidad);
 
 	t_new_pokemon* pokemon = new_pokemon(nombre, posx, posy, cantidad);
+	free(nombre);
 	enviar_mensaje(pokemon, conexion, NEW_POKEMON);
-
-	printf("Corrio re cheto");
 
 	//char* response = recibir_mensaje(conexion);
 	//printf("Mensaje devuelto: %s", response);
 
 	//por ahora no recibe nada
+}
+*/
 
-	terminar_programa(conexion, logger, config);
+void enviarNew(int conexion, char nombre[], char cuartoArg[], char quintoArg[], char sextoArg[]){
 
-	exit(0);
+	int posx = atoi(cuartoArg);
+	int posy = atoi(quintoArg);
+	int cantidad = atoi(sextoArg);
 
+	t_new_pokemon* pokemon = new_pokemon(nombre, posx, posy, cantidad);
+	enviar_mensaje(pokemon, conexion, NEW_POKEMON);
+}
+
+void enviarAppeared(int conexion, char nombre[], char cuartoArg[], char quintoArg[]){
+
+	int posx = atoi(cuartoArg);
+	int posy = atoi(quintoArg);
+
+	t_appeared_pokemon* pokemon = appeared_pokemon(nombre, posx, posy);
+	enviar_mensaje(pokemon, conexion, APPEARED_POKEMON);
+}
+
+void enviarCatch(int conexion, char nombre[], char cuartoArg[], char quintoArg[]){
+
+	int posx = atoi(cuartoArg);
+	int posy = atoi(quintoArg);
+
+	t_catch_pokemon* pokemon = catch_pokemon(nombre, posx, posy);
+	enviar_mensaje(pokemon, conexion, CATCH_POKEMON);
+}
+
+void enviarCaught(int conexion, char tercerArg[]){
+
+	int resultado = atoi(tercerArg);
+
+	t_caught_pokemon* pokemon = caught_pokemon(resultado);
+	enviar_mensaje(pokemon, conexion, CAUGHT_POKEMON);
+}
+
+void enviarGet(int conexion, char nombre[]){
+
+	t_get_pokemon* pokemon = get_pokemon(nombre);
+	enviar_mensaje(pokemon, conexion, CAUGHT_POKEMON);
 }
 
 void terminar_programa(int conexion, t_log* logger, t_config* config)
@@ -74,5 +216,7 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 	if (conexion != 0) {
 		liberar_conexion(conexion);
 	}
+
+	printf("Corrio re cheto\n");
 	printf("Finalizo programa.\n");
 }
--- game-boy/game-boy.h
+++ game-boy/game-boy.h
@@ -8,6 +8,21 @@
 #include<commons/config.h>
 #include<readline/readline.h>
 
+#define TAMANIO_MAXIMO_ARGUMENTO 20+1
+
+void verificarEntrada(int argc);
+int configuracionInicial(char* ip, char* puerto, t_log** logger, t_config** config, char *argv[]);
+void destinoBroker(char** ip, char** puerto, t_config* config);
+void destinoTeam(char** ip, char** puerto, t_config* config);
+void destinoGamecard(char** ip, char** puerto, t_config* config);
+void despacharMensaje(int conexion, int argc, char *argv[]);
+
+void enviarAlgoParaProbar(int conexion);	//PARA PROBAR A MANO
+void enviarNew(int conexion, char *nombre, char *cuartoArg, char *quintoArg, char *sextoArg);
+void enviarAppeared(int conexion, char *nombre, char *cuartoArg, char *quintoArg);
+void enviarCatch(int conexion, char *nombre, char *cuartoArg, char *quintoArg);
+void enviarCaught(int conexion, char *tercerArg);
+void enviarGet(int conexion, char *nombre);
 void terminar_programa(int, t_log*, t_config*);
 
 #endif
