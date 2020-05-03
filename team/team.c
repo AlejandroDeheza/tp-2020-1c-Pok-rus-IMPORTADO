a2c7b91064f737c14f62bc68d3c4ec0fafb7079c
utnso <ian.nehuen.martin@gmail.com>
2020-05-02 21:04:12 -0300

    Leo entrenadores del config y los cargo en una lista

--- team/team.c
+++ team/team.c
@@ -17,6 +17,43 @@
 #include "../utils/config.h"
 
 
+/*typedef struct nodo{
+	char *nombre;
+	struct nodo *sig;
+}Libro;*/
+
+typedef struct nodo
+{
+	int id_entrenador;
+	t_coordenadas coordenadas;
+	struct nodo *sig;
+} t_entrenador;
+
+
+t_entrenador *listaEntrenadores(t_entrenador *Lista){
+	Lista = NULL;
+	return Lista;
+}
+
+t_entrenador *agregarEntrenador(t_entrenador* Lista, int id_entrenador, int posx, int posy){
+	t_entrenador *nuevoEntrenador, *aux;
+	nuevoEntrenador= (t_entrenador*)malloc(sizeof(t_entrenador));
+	nuevoEntrenador->id_entrenador = id_entrenador;
+	nuevoEntrenador->coordenadas.posx = posx;
+	nuevoEntrenador->coordenadas.posy = posy;
+	nuevoEntrenador->sig = NULL;
+	if(Lista == NULL){
+		Lista = nuevoEntrenador;
+	}else{
+		aux = Lista;
+		while(aux->sig !=NULL){
+			aux = aux->sig;
+		}
+		aux->sig = nuevoEntrenador;
+	}
+	return Lista;
+}
+
 int main(void) {
 
 	int conexion;
@@ -28,6 +65,30 @@ int main(void) {
 	t_config* config;
 
 	config = leer_config("../team.config");
+
+	/*****************Leo del config y cargo los entrenadores a una lista*****************/
+	char* posicionesEntrenadores;
+	asignar_string_property(config, "POSICIONES_ENTRENADORES", &posicionesEntrenadores);
+	removeChar(posicionesEntrenadores, '[');
+	removeChar(posicionesEntrenadores, ']');
+	printf("Entrenadores quedo asi: %s\n", posicionesEntrenadores);
+
+	t_entrenador* Lista = listaEntrenadores(Lista);
+
+	char** posiciones = string_split(posicionesEntrenadores,",");
+
+	int i=0;
+	while(posiciones[i]!=NULL){
+		char** xy = string_split(posiciones[i],"|");
+		int posicionX = atoi(xy[0]);
+		int posicionY = atoi(xy[1]);
+		printf("Elemento %d, PosicionX: %d \n", i, posicionX);
+		printf("Elemento %d, PosicionY: %d \n", i, posicionY);
+		Lista=agregarEntrenador(Lista, i, posicionX, posicionY);
+		i++;
+	}
+
+	/*****************Leo del config y cargo los entrenadores a una lista*****************/
 
 	asignar_string_property(config, "IP_BROKER", &ip);
 	asignar_string_property(config, "PUERTO_BROKER", &puerto);
@@ -61,6 +122,16 @@ int main(void) {
 
 	exit(0);
 
+}
+
+void removeChar(char *str, char garbage) {
+
+    char *src, *dst;
+    for (src = dst = str; *src != '\0'; src++) {
+        *dst = *src;
+        if (*dst != garbage) dst++;
+    }
+    *dst = '\0';
 }
 
 void terminar_programa(int conexion, t_log* logger, t_config* config)
